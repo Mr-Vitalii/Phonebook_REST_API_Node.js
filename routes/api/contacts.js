967240bae77c91c5f4f973ca04c9a442af65537d
Mr-Vitalii <co4ckowoi.v@gmail.com>
2023-07-22 15:46:20 +0300

    created a backend for the PhoneBook

---
+++ routes/api/contacts.js
@@ -0,0 +1,24 @@
+
+const express = require("express");
+
+const router = express.Router();
+
+const ctrl = require("../../controllers/contacts");
+
+const { validateBody } = require("../../middlewares");
+
+const schemas = require("../../schemas/contacts");
+
+
+router.get("/", ctrl.getAll)
+
+router.get("/:id", ctrl.getById)
+
+router.post("/", validateBody(schemas.addSchema), ctrl.addContact);
+
+router.delete("/:id", ctrl.removeContact);
+
+router.put("/:id", validateBody(schemas.addSchema), ctrl.updateContact);
+
+module.exports = router;
+
