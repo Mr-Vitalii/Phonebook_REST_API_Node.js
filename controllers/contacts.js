fe3e39387ef8ed3db929d2128d499777c82a2069
Mr-Vitalii <co4ckowoi.v@gmail.com>
2023-07-26 17:21:21 +0300

    added methods for working with the database

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,15 +1,16 @@
-const contacts = require("../models/contacts")
+const { Contact } = require("../models/contacts/contact");
 
 const { HttpError, ctrlWrapper } = require("../helpers");
 
+
 const getAll = async (req, res) => {
-    const result = await contacts.listContacts();
+    const result = await Contact.find();
     res.json(result);
 }
 
 const getById = async (req, res) => {
     const { id } = req.params;
-    const result = await contacts.getContactById(id);
+    const result = await Contact.findById(id);
     if (!result) {
         throw HttpError(404, "Not found");
     }
@@ -17,28 +18,37 @@ const getById = async (req, res) => {
 }
 
 const addContact = async (req, res) => {
-    const result = await contacts.addContact(req.body);
+    const result = await Contact.create(req.body);
     res.status(201).json(result);
-}
-
-const removeContact = async (req, res) => {
-    const { id } = req.params;
-    const result = await contacts.removeContact(id);
-    if (!result) {
-        throw HttpError(404, "Not found");
-    }
-    res.json({
-        message: "contact deleted"
-    })
 }
 
 const updateContact = async (req, res) => {
     const { id } = req.params;
-    const result = await contacts.updateContact(id, req.body);
+    const result = await Contact.findByIdAndUpdate(id, req.body, { new: true });
     if (!result) {
         throw HttpError(404, "Not found");
     }
     res.json(result);
+}
+
+const updateFavorite = async (req, res) => {
+    const { id } = req.params;
+    const result = await Contact.findByIdAndUpdate(id, req.body, { new: true });
+    if (!result) {
+        throw HttpError(404, "Not found");
+    }
+    res.json(result);
+}
+
+const removeContact = async (req, res) => {
+    const { id } = req.params;
+    const result = await Contact.findByIdAndRemove(id);
+    if (!result) {
+        throw HttpError(404, "Not found");
+    }
+    res.json({
+        message: "Delete success"
+    })
 }
 
 module.exports = {
@@ -46,6 +56,6 @@ module.exports = {
     getById: ctrlWrapper(getById),
     addContact: ctrlWrapper(addContact),
     updateContact: ctrlWrapper(updateContact),
+    updateFavorite: ctrlWrapper(updateFavorite),
     removeContact: ctrlWrapper(removeContact),
-   
 }
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -5,20 +5,22 @@ const router = express.Router();
 
 const ctrl = require("../../controllers/contacts");
 
-const { validateBody } = require("../../middlewares");
+const { validateBody, isValidId } = require("../../middlewares");
 
-const schemas = require("../../schemas/contacts");
+const { schemas } = require("../../models/contacts/contact");
 
 
-router.get("/", ctrl.getAll)
+router.get("/", ctrl.getAll);
 
-router.get("/:id", ctrl.getById)
+router.get("/:id", isValidId, ctrl.getById);
 
 router.post("/", validateBody(schemas.addSchema), ctrl.addContact);
 
-router.delete("/:id", ctrl.removeContact);
-
 router.put("/:id", validateBody(schemas.addSchema), ctrl.updateContact);
+
+router.patch("/:id/favorite", isValidId, validateBody(schemas.updateFavoriteSchema), ctrl.updateFavorite);
+
+router.delete("/:id", ctrl.removeContact);
 
 module.exports = router;
 
