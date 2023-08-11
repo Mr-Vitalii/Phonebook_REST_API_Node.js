f6f1965fef70c1044dadfa007916f318b874ff1a
Mr-Vitalii <co4ckowoi.v@gmail.com>
2023-08-11 13:01:27 +0300

    added avatar

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const ctrl = require("../../controllers/auth");
 
-const { validateBody, authenticate } = require("../../middlewares");
+const { validateBody, authenticate, upload } = require("../../middlewares");
 
 const { schemas } = require("../../models/user");
 
@@ -20,5 +20,7 @@ router.get("/current", authenticate, ctrl.getCurrent);
 router.patch("/users", authenticate, validateBody(schemas.updateStatusSchema), ctrl.updateSubscriptionStatus);
 
 router.post("/logout", authenticate, ctrl.logout);
+
+router.patch("/avatars", authenticate, upload.single("avatar"), ctrl.updateAvatar);
 
 module.exports = router;
